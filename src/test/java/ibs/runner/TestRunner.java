c9c6e7ef77b4c35bc282da1423ae9fe619d13f74
Trishin Nikita <trishinnikita98@gmail.com>
2025-10-06 14:13:49 +0300

    переменная tags

--- src/test/java/ibs/runner/TestRunner.java
+++ src/test/java/ibs/runner/TestRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.core.options.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
+
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ibs.steps"),
         @ConfigurationParameter(
